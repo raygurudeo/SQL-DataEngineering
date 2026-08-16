9ee6a8cb23c053cec5102d390b0962f71a3e5e9e
raygurudeo <raygurudeo@gmail.com>
2026-08-16 10:11:19 +0530

    03_sql_basics_select_where_orderby.sql updated.

--- 03_sql_basics_select_where_orderby.sql
+++ 03_sql_basics_select_where_orderby.sql
@@ -22,3 +22,27 @@ left join hospital.admissions a on p.patient_id = a.patient_id where a.patient_i
 -- Show patients and their doctors (via admissions) 
 select p.patient_id, p.first_name, p.last_name, d.first_name as Doctor_Name, d.specialty from HOSPITALDB.HOSPITAL.PATIENTS p join hospital.admissions a on p.patient_id=a.patient_id join hospital.doctors d on a.doctor_id = d.doctor_id;
 
+-- List each admission with patient name and doctor name <<<Example for inner join or only join. As we want each admission details with patient name and doctor name. SO in that case we don't want a admission without patient name and doctor name so I used inner join. If we need all admission even if doctor or patient details are not present for that admission then we can use left join>>>
+select a.admission_id, a.admission_date, p.first_name || ' ' || p.last_name as Patient_Name, d.first_name || ' ' || d.last_name as Doctor_Name from hospital.admissions a
+join patients p on a.patient_id = p.patient_id
+join doctors d on a.doctor_id = d.doctor_id;
+
+-- Show all bills with patient name and insurance provider.
+select a.admission_id,
+b.billing_id,
+p.first_name || ' ' || p.last_name as Patient_Name,
+b.insurance_provider, 
+b.total_charges, 
+b.amount_paid_by_insurance, 
+b.payment_status from hospital.billings b
+join hospital.admissions a on b.admission_id = a.admission_id
+join hospital.patients p on a.patient_id = p.patient_id;
+
+-- Find doctors who treated patients from California
+select d.first_name || ' ' || d.last_name as Doctor_Name,
+p.first_name || ' ' || p.last_name as Patient_Name,
+p.state
+from hospital.doctors d
+join hospital.admissions a on d.doctor_id=a.doctor_id
+join hospital.patients p on p.patient_id=a.patient_id
+where p.state='CA';
